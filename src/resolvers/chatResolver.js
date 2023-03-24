a5ba838edcda50ae7cc9af64919b21abb71c53b8
Miguel Puentes <mipuentesc@unal.edu.co>
2023-03-24 14:09:34 -0500

    Conditions profile added

--- src/resolvers/chatResolver.js
+++ src/resolvers/chatResolver.js
@@ -4,23 +4,51 @@ import { URLchat, URLprofile } from '../server';
 const URL = URLchat
 
 export const chatResolvers = {
+    // Gets
     Query:{
-        // chat gets
         getChatId: (_, { id }) => Promise.resolve(generalRequest(`${URL}/chat/${id}`, '')).then((value) => { return value}),         // get chat by ID
         getChats: (_) => Promise.resolve(generalRequest(`${URL}/chat`, '')).then((value) => { return value}),                      // get all chats
         getChatUser: (_, { name }) => Promise.resolve(generalRequest(`${URL}/chat/${name}`, '')).then((value) => { return value}) // get chat by user
     },
+    // Filling sender and receiver field in chat with connection to profile
     chat:{
-        sender:({sender}) => {return Promise.resolve(generalRequest(`${URLprofile}/profile/get/${sender}`, '')).then((value) => { return value.profiles})},         // get with a ID},
-        receiver:({receiver}) => {return Promise.resolve(generalRequest(`${URLprofile}/profile/get/${receiver}`, '')).then((value) => { return value.profiles})}         // get with a ID},
+        sender:({sender}) => {return Promise.resolve(generalRequest(`${URLprofile}/profile/get/${sender}`, '')).then((value) => { return value.profiles})},         
+        receiver:({receiver}) => {return Promise.resolve(generalRequest(`${URLprofile}/profile/get/${receiver}`, '')).then((value) => { return value.profiles})}        
     },
     messageSchema:{
-        sender:({sender}) => {return Promise.resolve(generalRequest(`${URLprofile}/profile/get/${sender}`, '')).then((value) => { return value.profiles})}         // get with a ID},
+        sender:({sender}) => {return Promise.resolve(generalRequest(`${URLprofile}/profile/get/${sender}`, '')).then((value) => { return value.profiles})}        
     },
+    // Update, Create and Delete
     Mutation:{
-        // chat create and update
-        // Falta añadir integridad de la información.
-        createChat: (_, { chat }) => Promise.resolve(generalRequest(`${URL}/chat/`, 'POST', chat)).then((value) => { return value}), //Create a chat
-        addMessage: (_, { chat }) => Promise.resolve(generalRequest(`${URL}/chat/`, 'PATCH', chat)).then((value) => { return value})      //Add a message
+        // Creating and chat
+        createChat: (_, { chat }) => { 
+            return (async () => {
+                // querys conditions definition
+                let senderQuery =  await generalRequest(`${URLprofile}/profile/get/${chat.sender}`, '').then((value) => {return value.profiles});
+                let receiverQuery =  await generalRequest(`${URLprofile}/profile/get/${chat.receiver}`, '').then((value) => { return value.profiles});
+                let messageQuery =  await generalRequest(`${URLprofile}/profile/get/${chat.messages[0].sender}`, '').then((value) => { return value.profiles});
+
+                // conditions
+                if (senderQuery == null || receiverQuery == null || messageQuery == null){return "Chat failed to add, invalid profile"}
+                if (messageQuery._id == senderQuery._id || messageQuery._id == receiverQuery._id){return Promise.resolve(generalRequest(`${URL}/chat/`, 'POST', chat)).then((value) => { return value})}
+                // else
+                return "message sender not match with this chat" 
+              })()
+        }, 
+        // add message to chat
+        addMessage: (_, { chat }) => {
+            return (async () => {
+                // querys conditions definition
+                let senderQuery =  await generalRequest(`${URLprofile}/profile/get/${chat.sender}`, '').then((value) => {return value.profiles});
+                let receiverQuery =  await generalRequest(`${URLprofile}/profile/get/${chat.receiver}`, '').then((value) => { return value.profiles});
+                let messageQuery =  await generalRequest(`${URLprofile}/profile/get/${chat.messages[0].sender}`, '').then((value) => { return value.profiles});
+
+                // conditions
+                if (senderQuery == null || receiverQuery == null || messageQuery == null){return "Message failed to add, invalid profile"}
+                if (messageQuery._id == senderQuery._id || messageQuery._id == receiverQuery._id){return Promise.resolve(generalRequest(`${URL}/chat/`, 'PATCH', chat)).then((value) => { return value})}
+                // else
+                return "message sender not match with this chat" 
+            })()
+        }  
     }
 };
